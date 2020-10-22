7c4a534c7dcd9228ccdc9604a45b9be459c873d8
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2020-10-22 22:03:59 +0200

    Fix imports and formatting

--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ setup(
     package_dir={"": "src"},
     packages=find_packages("src"),
     install_requires=[
+        "beautifulsoup4",
+        "dominate>=2.4.0",
+        "scikit-image",
         "torch>=1.4.0",
         "torchvision>=0.5.0",
-        "dominate>=2.4.0",
         "visdom>=0.1.8.8",
     ]
 )
